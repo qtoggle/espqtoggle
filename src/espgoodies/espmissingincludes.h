c4005b7b3c61b3f47e2bff7ebda7c9a252cf279b
Calin Crisan <ccrisan@gmail.com>
2019-10-01 22:57:16 +0300

    Remove unneeded stuff from espmissingincludes.h

--- src/espgoodies/espmissingincludes.h
+++ src/espgoodies/espmissingincludes.h
@@ -24,8 +24,6 @@
 
 void ets_isr_mask(unsigned intr);
 void ets_isr_unmask(unsigned intr);
-int ets_sprintf(char *str, const char *format, ...)  __attribute__ ((format (printf, 2, 3)));
-int ets_snprintf(char *str, size_t size, const char *format, ...) __attribute__ ((format (printf, 3, 4)));
 int ets_vsnprintf(char *str, size_t size, const char *format, va_list args) __attribute__ ((format (printf, 3, 0)));
 
 #define snprintf    ets_snprintf
@@ -34,10 +32,5 @@ int ets_vsnprintf(char *str, size_t size, const char *format, va_list args) __at
 
 typedef signed short        int16;
 typedef signed long long    int64;
-
-#ifndef FALSE
-#define FALSE 0
-#define TRUE 1
-#endif
 
 #endif /* _ESPGOODIES_ESPMISSINGINCLUDES_H */
