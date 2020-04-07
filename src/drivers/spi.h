3e1f2d20834035a9e97730f4d4d837ecc2b05fae
Calin Crisan <ccrisan@gmail.com>
2020-04-07 20:01:43 +0300

    API: /rawio: add support for hspi

--- src/drivers/spi.h
+++ src/drivers/spi.h
@@ -35,6 +35,7 @@
 
 
 ICACHE_FLASH_ATTR void              spi_setup(uint8 bit_order, bool cpol, bool cpha, uint32 freq);
+ICACHE_FLASH_ATTR bool              spi_get_current_setup(uint8 *bit_order, bool *cpol, bool *cpha, uint32 *freq);
 ICACHE_FLASH_ATTR void              spi_transfer(uint8 *out_buff, uint8 *in_buff, uint32 len);
 ICACHE_FLASH_ATTR uint8             spi_transfer_byte(uint8 byte);
 
