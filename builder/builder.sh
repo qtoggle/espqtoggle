9d6a5bfd1453828f6b5774b1f713304a43cf5cbd
Calin Crisan <ccrisan@gmail.com>
2020-07-03 23:56:06 +0300

    Update CI & building mechanism

--- builder/builder.sh
+++ builder/builder.sh
@@ -20,35 +20,41 @@ set -ae
 
 # setup AWS
 S3CFG="/root/.s3cfg"
-if ! [ -f ${S3CFG} ]; then
+if ! [[ -f ${S3CFG} ]]; then
     echo "[default]" > ${S3CFG}
 fi
-if [ -n "${AWS_ACCESS_KEY}" ]; then
+if [[ -n "${AWS_ACCESS_KEY}" ]]; then
     echo "access_key = ${AWS_ACCESS_KEY}" > ${S3CFG}
 fi
-if [ -n "${AWS_SECRET_KEY}" ]; then
+if [[ -n "${AWS_SECRET_KEY}" ]]; then
     echo "secret_key = ${AWS_SECRET_KEY}" > ${S3CFG}
 fi
 
-function clone() {
+if [[ -n "${EB_REPO}" ]]; then
     git clone ${EB_REPO} ${SRC_DIR}
     cd ${SRC_DIR}
     git checkout ${EB_CHECKOUT}
-}
-
-function build() {
+else
     cd ${SRC_DIR}
-    make -j4
-}
+fi
 
-function release() {
-    CONFIG_NAME=$1
-    
-    echo "**** preparing configuration ${CONFIG_NAME} ****"
-    mkdir -p ${RELEASE_DIR}/${CONFIG_NAME}
-    cp ${SRC_DIR}/build/user*.bin ${RELEASE_DIR}/${CONFIG_NAME}
-    cp ${SRC_DIR}/build/firmware.bin ${RELEASE_DIR}/${CONFIG_NAME}
-    
+sed -ri "s/(FW_VERSION *)\"0.0.0-unknown.0\"/\1\"${EB_VERSION}\"/" ${SRC_DIR}/src/ver.h
+
+make clean
+make -j4
+
+if [[ -n "${AWS_ACCESS_KEY}" && 
+      -n "${AWS_SECRET_KEY}" && 
+      -n "${AWS_BUCKET}" && 
+      -n "${AWS_FOLDER}" && 
+      "${DO_RELEASE}" == "true" ]]; then
+
+    echo "uploading release to AWS"
+
+    mkdir -p ${RELEASE_DIR}
+    cp ${SRC_DIR}/build/user*.bin ${RELEASE_DIR}
+    cp ${SRC_DIR}/build/firmware.bin ${RELEASE_DIR}
+
     if [[ "${EB_VERSION}" =~ beta.[0-9]+$ ]]; then  # beta version
         latest_file="latest_beta"
     elif [[ "${EB_VERSION}" =~ alpha.[0-9]+$ ]]; then  # alpha version
@@ -63,36 +69,14 @@ function release() {
     
     cp ${RELEASE_DIR}/${latest_file} ${RELEASE_DIR}/latest
 
-    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/${CONFIG_NAME}/user1.bin \
-                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${CONFIG_NAME}/${EB_VERSION}/user1.bin
-    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/${CONFIG_NAME}/user2.bin \
-                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${CONFIG_NAME}/${EB_VERSION}/user2.bin
-    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/${CONFIG_NAME}/firmware.bin \
-                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${CONFIG_NAME}/${EB_VERSION}/firmware.bin
+    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/user1.bin \
+                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${EB_VERSION}/user1.bin
+    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/user2.bin \
+                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${EB_VERSION}/user2.bin
+    s3cmd put -P --guess-mime-type ${RELEASE_DIR}/firmware.bin \
+                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${EB_VERSION}/firmware.bin
     s3cmd put -P --guess-mime-type ${RELEASE_DIR}/${latest_file} \
-                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${CONFIG_NAME}/${latest_file}
+                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${latest_file}
     s3cmd put -P --guess-mime-type ${RELEASE_DIR}/latest \
-                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/${CONFIG_NAME}/latest
-}
-
-function clean() {
-    cd ${SRC_DIR}
-    make clean
-}
-
-if [ -n "${EB_REPO}" ]; then
-    clone
+                                   s3://${AWS_BUCKET}/${AWS_FOLDER}/latest
 fi
-
-if [ -d "${SRC_DIR}" ]; then
-    sed -ri "s/(FW_VERSION *)\"0.0.0-unknown.0\"/\1\"${EB_VERSION}\"/" ${SRC_DIR}/src/ver.h
-fi
-
-for conf in ${EB_CONF_FILES}; do
-    clean
-    echo -e "\n\n**** building configuration ${conf} ****"
-    (source ${conf} && build)
-    if [[ -n "${AWS_ACCESS_KEY}" && -n "${AWS_SECRET_KEY}" && -n "${AWS_BUCKET}" && -n "${AWS_FOLDER}" && "${DO_RELEASE}" == "true" ]]; then
-        release $(cat ${SRC_DIR}/build/.config_name)
-    fi
-done
