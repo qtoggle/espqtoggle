1f5899efa896aebd751a37f19ce2b1a9909ffbef
Calin Crisan <ccrisan@gmail.com>
2020-07-04 16:53:30 +0300

    Update builder

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-SRC_DIR=${SRC_DIR:-/opt/espqtoggle}
-RELEASE_DIR=${RELEASE_DIR:-/opt/release}
-export DEBUG=${DEBUG:-false}
-
-# deduce version
-if [ -z "${EB_VERSION}" ]; then
-    if [ "${EB_CHECKOUT}" == "master" ]; then
-        EB_VERSION="latest"
-    elif [[ "$EB_CHECKOUT" =~ ^[a-f0-9]{40}$ ]]; then  # special commit id case
-        EB_VERSION=git${EB_CHECKOUT::7}
-    else
-        EB_VERSION=${EB_CHECKOUT}
-    fi
+if [[ -z "${SRC_DIR}" ]]; then
+    echo "SRC_DIR not defined"
+    exit 1
 fi
 
+cd ${SRC_DIR}
+RELEASE_DIR=${RELEASE_DIR:-/opt/release}
+
+if [[ -z "${VERSION}" ]]; then
+    VERSION=$(/opt/makever.sh)
+fi
+
+echo "Using version ${VERSION}"
 
 set -ae
 
@@ -30,15 +28,7 @@ if [[ -n "${AWS_SECRET_KEY}" ]]; then
     echo "secret_key = ${AWS_SECRET_KEY}" > ${S3CFG}
 fi
 
-if [[ -n "${EB_REPO}" ]]; then
-    git clone ${EB_REPO} ${SRC_DIR}
-    cd ${SRC_DIR}
-    git checkout ${EB_CHECKOUT}
-else
-    cd ${SRC_DIR}
-fi
-
-sed -ri "s/(FW_VERSION *)\"0.0.0-unknown.0\"/\1\"${EB_VERSION}\"/" ${SRC_DIR}/src/ver.h
+sed -ri "s/0.0.0-unknown.0/${VERSION}/" ${SRC_DIR}/src/ver.h
 
 make clean
 make -j4
@@ -49,15 +39,15 @@ if [[ -n "${AWS_ACCESS_KEY}" &&
       -n "${AWS_FOLDER}" && 
       "${DO_RELEASE}" == "true" ]]; then
 
-    echo "uploading release to AWS"
+    echo "Uploading release to AWS"
 
     mkdir -p ${RELEASE_DIR}
     cp ${SRC_DIR}/build/user*.bin ${RELEASE_DIR}
     cp ${SRC_DIR}/build/firmware.bin ${RELEASE_DIR}
 
-    if [[ "${EB_VERSION}" =~ beta.[0-9]+$ ]]; then  # beta version
+    if [[ "${VERSION}" =~ beta.[0-9]+$ ]]; then  # beta version
         latest_file="latest_beta"
-    elif [[ "${EB_VERSION}" =~ alpha.[0-9]+$ ]]; then  # alpha version
+    elif [[ "${VERSION}" =~ alpha.[0-9]+$ ]]; then  # alpha version
         latest_file="latest_alpha"
     else  # stable version
         latest_file="latest_stable"
